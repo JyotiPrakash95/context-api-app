43a9d1dbb8be2a3341c15b9143891a4aa2bd0cee
Jyoti Prakash <jyotiprakash0737@gmail.com>
2024-01-06 18:41:46 +0530

    finalize app

--- color-app/src/components/GrendChild.js
+++ color-app/src/components/GrendChild.js
@@ -1,11 +1,16 @@
 import { useContext } from "react";
 import { colorContext } from "../context";
-function GrendPerent() {
+const GrendPerent =()=> {
     const color = useContext(colorContext);
     console.log(color);
   return (
     <>
-          <h2 style={{ color: color }}>Color: {color}</h2>
+        <h2 style={{ color: color }}>Color: {color}</h2>
+        
+        {/* this is the way how class component use context */}
+      {/* <colorContext.Consumer>
+        {(value) => <p style={{ color: value.color }}>color: {value.color}</p>}
+      </colorContext.Consumer> */}
     </>
   );
 };
